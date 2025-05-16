d2317af0f3de42cbd172a2bf3f6c0761e98f9e79
Scott Bedard <scottbedard90@gmail.com>
2025-05-15 21:33:49 -0500

    cleanup

--- index.mjs
+++ index.mjs
@@ -1,17 +1,37 @@
+// 379,212 <- original two dictionaries
+// 379,869 <- mit
+// 381,193 <- some random github repo
+// 387,978 <- university of michigan
+// 621,122 <- kaggle
+
 import fs from 'node:fs'
 
 const dictionary = fs.readFileSync('dictionary.txt', 'utf8').split('\n')
 
-const awesome = "bishhhhhh"
+// const add = fs.readFileSync('add.txt', 'utf8').split('\n')
+
+// const combined = add
+//   .concat(dictionary)
+//   .filter(str => str.match(/^[a-zA-Z]+$/))
+//   .sort()
+//   .map(str => str.toLowerCase())
+
+// const unique = [...new Set(combined)]
+
+// fs.writeFileSync('combined.txt', unique.join('\n'))
+
 const cipher = "enpiadh"
+
+const wordLength = 8
+
+const requiredLetters = 'i'
+
+const forbiddenLetters = ''
+
+const startsWith = 'pi'
 
 function checkifwordisvalid (word) {      
   const length = word.length
-
-  if (length !== 7) {
-    return false
-  }
-
 
   for (let i = 0; i < length; i++) {
     const currentletter = word[i]
@@ -20,30 +40,44 @@ function checkifwordisvalid (word) {
       return false
     }
   }
+
   return true
 }
 
 for (const word of dictionary) {
-  // if (word.length != 5 && word.legnth != 8 && word.length != 9) {
-  //   continue
-  // }
-
-  // if (word.length != 9){
-  //   continue
-  // }
-
-  if (word.includes("i") === false) {
+  if (wordLength && word.length !== wordLength){
     continue
   }
 
-  if (word.startsWith("he") === false) {
+  if (startsWith && !word.startsWith(startsWith)){
     continue
   }
 
-  // const firstletter = word[0] 
-  // if (firstletter != "c") {
-  //   continue
-  // }
+  let hasRequiredLetters = true
+
+  for (const letter of requiredLetters.split('')){
+    if (!word.includes(letter)){
+      hasRequiredLetters = false
+      break
+    }
+  }
+
+  if (!hasRequiredLetters) {
+    continue
+  }
+
+  let hasForbiddenLetters = false
+
+  for (const letter of forbiddenLetters.split('')){
+    if (word.includes(letter)){
+      hasForbiddenLetters = true
+      break
+    }
+  }
+
+  if (hasForbiddenLetters) {
+    continue
+  }
 
   if (checkifwordisvalid(word)) {
     console.log(word)
